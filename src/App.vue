4686a85c56a73f741494a4b90f91b75076f6acb4
Jonathanhoejtoft <jonathanhoejtoft@gmail.com>
2020-04-21 18:02:21 +0200

    Added style to the employee list

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,13 @@ export default {
   },
   data(){
     return{
-      employees:[]
+      employees:[{
+            "id": "1",
+            "employee_name": "Tiger Nixon",
+            "employee_salary": "320800",
+            "employee_age": "61",
+            "profile_image": ""
+        }]
     }
   },
 created(){
--- src/components/EmployeeItem.vue
+++ src/components/EmployeeItem.vue
@@ -1,38 +1,46 @@
 <template>
     <div>
         <v-container>
+           
+
                 <div class="employee-card">
             <v-row>
-                <v-col cols="3">
+                <v-col cols="3" >
                     <div class="right-section">
                         <div v-if='employee.profile_image === "" '>
-                        <v-img src="../assets/avatar.png"></v-img>
+                        <v-img style="border-radius:50%" src="../assets/avatar.png"></v-img>
                         </div>
                         <div v-else>
                             <v-img v-bind:src="employee.profile_image"></v-img>
                         </div>
                     </div>
                 </v-col>
-                <v-col cols="6">
-                    <v-row>
+                <v-col cols="6" style="margin-left:15px">
+                    <v-row class="empName" >
                         <h3>{{employee.employee_name}}</h3>
                     </v-row>
-                    <v-row>
-                        <div class="empAge">
-                            <p>Age: </p>
-                            <p>{{employee.employee_age}}</p>
+                    <v-divider class="empDivider"></v-divider>
+                      <v-row style="margin-bottom:10px">
+                        <div class="empAge c_chip">
+                            <b>{{employee.employee_age}}</b>
+                            <p> years </p>
+                            <v-icon>mdi-cake-variant</v-icon>
                         </div>
                     </v-row>
                     <v-row>
-                        <div class="empSalary">                       
-                            <p>Salary: </p>                       
-                            <p>{{employee.employee_salary}}</p>
-                       
+                        <div class="empSalary c_chip">
+                            <b>{{employee.employee_salary}}</b>
+                            <p> income </p>                 
+                            <v-icon>mdi-work</v-icon>      
+                            <i class="fas fa-hand-holding-usd"></i>                       
+
                         </div>
                     </v-row>
                 </v-col>
             </v-row>
-                </div>             
+                </div> 
+           
+                       
         </v-container>
 
     </div>
@@ -46,14 +54,29 @@ export default {
 </script>
 <style scoped>
 .right-section{
-    background: red;
+    margin-left: 15px;
 }
 .employee-card{
-    width:60%;
+    width:40%;
     margin: auto;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 .empAge p,.empSalary p{
     display: inline;
+}
+.empAge i, .empSalary i{
+    font-size:20px;
+}
+.empName{
+    margin-bottom: 15px ;
+}
+.empDivider{
+    margin-bottom: 15px;
+}
+
+.c_chip{
+    background: #6cc0f3;
+    border-radius: 25px;
+    padding: 10px;
 }
 </style>
