6bf72131ff4300b926906877274e5c5b1b0bdfdb
Jonathanhoejtoft <jonathanhoejtoft@gmail.com>
2020-04-21 16:37:31 +0200

    First stage of styling

--- src/components/EmployeeItem.vue
+++ src/components/EmployeeItem.vue
@@ -1,15 +1,38 @@
 <template>
     <div>
         <v-container>
+                <div class="employee-card">
             <v-row>
-                <p>{{employee.employee_name}}</p>
+                <v-col cols="3">
+                    <div class="right-section">
+                        <div v-if='employee.profile_image === "" '>
+                        <v-img src="../assets/avatar.png"></v-img>
+                        </div>
+                        <div v-else>
+                            <v-img v-bind:src="employee.profile_image"></v-img>
+                        </div>
+                    </div>
+                </v-col>
+                <v-col cols="6">
+                    <v-row>
+                        <h3>{{employee.employee_name}}</h3>
+                    </v-row>
+                    <v-row>
+                        <div class="empAge">
+                            <p>Age: </p>
+                            <p>{{employee.employee_age}}</p>
+                        </div>
+                    </v-row>
+                    <v-row>
+                        <div class="empSalary">                       
+                            <p>Salary: </p>                       
+                            <p>{{employee.employee_salary}}</p>
+                       
+                        </div>
+                    </v-row>
+                </v-col>
             </v-row>
-            <v-row>
-                <p>{{employee.employee_age}}</p>
-            </v-row>
-            <v-row>
-                <p>{{employee.employee_salary}}</p>
-            </v-row>
+                </div>             
         </v-container>
 
     </div>
@@ -22,5 +45,15 @@ export default {
 }
 </script>
 <style scoped>
-
+.right-section{
+    background: red;
+}
+.employee-card{
+    width:60%;
+    margin: auto;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.empAge p,.empSalary p{
+    display: inline;
+}
 </style>
