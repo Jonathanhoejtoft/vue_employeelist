e17c5672a70f4b3577867630036ad38801ec37bd
Jonathanhoejtoft <jonathanhoejtoft@gmail.com>
2020-04-21 18:08:27 +0200

    Removed dummy data

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,7 @@ export default {
   },
   data(){
     return{
-      employees:[{
-            "id": "1",
-            "employee_name": "Tiger Nixon",
-            "employee_salary": "320800",
-            "employee_age": "61",
-            "profile_image": ""
-        }]
+      employees:[]
     }
   },
 created(){
