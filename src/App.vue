6c04fb272642f18d0d116d9d46e6b7099df393cd
Jonathanhoejtoft <jonathanhoejtoft@gmail.com>
2020-04-21 20:07:30 +0200

    Added toggle view for employees - datatable and customview

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="app">
     <Header />
+    <button class="toggleBtn" @click="this.toggleView">Toggleview - {{this.viewType}}</button>
+  <div v-if="tableView === false">
   <Employees v-bind:employees="employees" />
+  </div>
+  <div v-else>
+    <v-container>
+      <v-data-table
+      :headers="headers"
+      :items="employeesTable"
+      :sort-by="['employee_salary', 'employee_age']"
+      :sort-desc="[false, true]"
+      multi-sort
+      class="elevation-1"
+    ></v-data-table>
+    </v-container>
+  </div>
   </div>
 </template>
 
@@ -17,12 +32,41 @@ export default {
   },
   data(){
     return{
-      employees:[]
+      headers: [
+          {
+            text: 'Employees',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'employee_name', value: 'employee_name' },
+          { text: 'employee_salary', value: 'employee_salary' },
+          { text: 'employee_age', value: 'employee_age' },
+          { text: 'profile_image', value: 'profile_image' },
+        ],
+        employeesTable: [],
+      employees:[],
+      tableView:true,
+      viewType:''
+    }
+  },
+  methods:{
+    toggleView(){
+      this.tableView = !this.tableView;
+      this.viewType = this.tableView == true ? 'datatable' : 'customview';
+      console.log(this.tableView);
     }
   },
 created(){
   axios.get('http://dummy.restapiexample.com/api/v1/employees')
   .then(res => this.employees = res.data.data)
+  .catch(err => console.log(err));
+},
+mounted(){
+  this.viewType = this.tableView == true ? 'datatable' : 'customview';
+
+  axios.get('http://dummy.restapiexample.com/api/v1/employees')
+  .then(res => this.employeesTable = res.data.data)
   .catch(err => console.log(err));
 }
 };
@@ -39,5 +83,14 @@ created(){
 body{
   margin:0;
   padding: 0;
+}
+.toggleBtn{
+  background: #39841a;
+    color: white;
+    padding: 10px;
+    margin-top: 10px;
+}
+.toggleBtn:hover{
+  background: #55af30;
 }
 </style>
